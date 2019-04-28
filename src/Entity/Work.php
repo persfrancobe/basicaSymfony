ddb318c7bbf4e9ce9eba06f2dbc9d2efc111fe26
Kalantarian Ehsan <pers.franco.be@gmail.com>
2019-04-28 21:59:48 +0200

    finish

--- src/Entity/Work.php
+++ src/Entity/Work.php
@@ -6,6 +6,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\WorkRepository")
@@ -51,6 +52,7 @@ class Work
     private $slug;
 
     /**
+     *
      * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="works")
      */
     private $user;
